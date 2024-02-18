1f72997cfc681aa90e53005ce025e1313b651b54
nikitakod <51436758+nikitakod@users.noreply.github.com>
2024-02-19 00:32:29 +0530

    Update GFG.java

--- GFG.java
+++ GFG.java
@@ -2,37 +2,9 @@ import java.io.*;
 
 class GFG1 {
 
-    // Function to find the biggest of three numbers
-    static int biggestOfThree(int x, int y, int z) {
-        return z > (x > y ? x : y) ? z : ((x > y) ? x : y);
-    }
 
-    // Main driver function
     public static void main(String[] args) {
-        // Declaring variables for 3 numbers
-        int a, b, c;
-
-        // Variable holding the largest number
-        int largest;
-        a = 5;
-        b = 10;
-        c = 3;
-
-        // Introduce an unused variable
-        int unusedVariable = 0;
-
-        // Initialize 'b' with a non-integer value (e.g., "hello")
-        // Uncomment the line below to trigger a type-related error
-        // b = "hello";
-
-        // Calling the above function in main
-        largest = biggestOfThree(a, b, c);
-
-        // Printing the largest number
-        System.out.println(largest + " is the largest number.");
-
-        // Introduce an unhandled exception
-        // Uncomment the line below to trigger an "unhandled exception" issue
-        // throw new RuntimeException("Oops! Something went wrong.");
+        System.out.println("DEMO");
+       
     }
 }
